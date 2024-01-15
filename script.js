3100f17ec2c02a31b06452b60eb646b076cfce42
oways2008 <135352533+oways2008@users.noreply.github.com>
2024-01-15 05:19:46 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let valuedisplays = document.querySelectorAll(".num");
+let interval = 5000;
+
+valuedisplays.forEach(valuedisplay =>{
+    let startvalue = 0;
+    let endvalue = parseInt(valuedisplay.getAttribute("data-val"));
+    let duration = Math.floor(interval / endvalue);
+    let counter = setInterval(function() {
+     startvalue += 1; 
+     valuedisplay.textContent = startvalue ;
+     if (startvalue == endvalue){
+        clearInterval(counter);
+
+     }  
+    },duration);
+})
